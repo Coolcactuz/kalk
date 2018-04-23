38ed5374f76f749694a9d7bed9b5830099130172
TheAlchemist97 <vettorelloniccolo97@gmail.com>
2018-04-23 13:45:31 +0200

    parse function

--- Back-end/main.cpp
+++ Back-end/main.cpp
@@ -7,18 +7,7 @@
 using namespace std;
 
 int main() {
-    /*C_cartesiano*a=new C_cartesiano(7,4);
-    C_cartesiano*b=new C_cartesiano(7,4);
-    C_cartesiano*c=new C_cartesiano(*(*a+*b));
-    delete a;
-    delete b;
-    Complesso*d=c->converti();
-    delete c;
-    C_polare*f=new C_polare(5,-45);
-    C_polare*g=new C_polare(*(*f-*d));
-    cout<<*g<<endl;*/
-    Componente* c= new Resistore("r1",12);
-
+  
 
     return 0;
 }
