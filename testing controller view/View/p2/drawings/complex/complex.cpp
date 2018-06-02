fb9f01b7b515046a3c027619bbe79b1ad04ec36c
nick <nick@nic-fuji.nick>
2018-06-02 18:54:22 +0200

    rappresentazione complessi

--- testing controller view/View/p2/drawings/complex/complex.cpp
+++ testing controller view/View/p2/drawings/complex/complex.cpp
@@ -8,15 +8,16 @@
 
 using namespace std;
 
-class Widget : public QWidget
-{
+class Widget : public QWidget{
+
 public:
 
     void paintEvent(QPaintEvent* event)
     {
         QPainter p;
-
         p.begin(this);
+
+        //assi cartesiani
 
         p.setRenderHint(QPainter::Antialiasing);
         p.setPen(QPen(Qt::black, 5, Qt::SolidLine));
@@ -26,71 +27,123 @@ public:
 
         p.drawText((QWidget::width() / 2) + 5, (QWidget::height() / 2) + 20, "0");
 
+        //numero complesso cartesiano
 
-        double reale = -0.481;
-        double immaginaria = 6.594;
+        double reale = -0.234;
+        double immaginaria = 0.31;
 
-        if((reale >= 1 || reale <= -1) && (immaginaria >= 1 || immaginaria <=-1)){
+        //indici e posizione parte reale
 
-          int intero_reale = abs(static_cast<int>(reale));
-          int intero_imm = abs(static_cast<int>(immaginaria));
+        int x = 0;
+        int ipx = -1;
+        int inx = 1;
 
-          p.drawText((QWidget::width() / 2) + 5, (QWidget::height() / 2) + 20, "0");
+        double abs_reale = abs(reale);
 
-            p.drawText(
-              ((QWidget::width() * 11) / 12),
-              (QWidget::height() / 2) + 20,
-              QString::number(intero_reale * 2)
-            );
-            p.drawText(
-              (QWidget::width() / 12),
-              (QWidget::height() / 2) + 20,
-              QString::number(intero_reale * -2)
-            );
+        if(abs_reale >= 1){
 
-            p.drawText(
-              QWidget::width() / 2 + 5,
-              QWidget::height() / 12,
-              QString::number(intero_imm * 2)
-            );
-            p.drawText(
-              QWidget::width() / 2 + 5,
-              (QWidget::height() * 11) / 12,
-              QString::number(intero_imm * -2)
-            );
+          int aux1 = static_cast<int>(abs_reale);
 
-            int x = 0;
-            int y = 0;
+          ipx = aux1*2;
+          inx = aux1*(-2);
 
-            int sizex = ((QWidget::width() * 11) / 12) - QWidget::width()/2;
-            int sizey = ((QWidget::height() * 11) / 12) - QWidget::height()/2;
+          if(reale > 0)
+            x = ((QWidget::width()*5)/8) + QWidget::width()/12;
+          else
+            x = ((QWidget::width()*5)/24) + QWidget::width()/12;
+        }
+        else if (abs_reale == 0){
 
-            if(reale > 0)
-              x = QWidget::width()/2 + sizex/2;
-            else
-              x = QWidget::width()/12 + sizex/2;
+          inx = -1;
+          ipx = 1;
 
-            if(immaginaria > 0)
-              y = QWidget::height()/12 + sizey/2;
-            else
-              y = QWidget::height()/2 + sizey/2;
+          cout << "qui1" << endl;
 
-            p.setPen(QPen(Qt::green, 12, Qt::SolidLine, Qt::RoundCap));
-            p.drawPoint(x, y);
 
-            p.setPen(QPen(Qt::black, 5, Qt::SolidLine));
-            p.drawText(x + 10,
-              y + 10,
-              QString('(').append(QString::number(reale)).append(QString(',')).append(QString::number(immaginaria)).append(QString(')'))
-            );
+          x = QWidget::width()/2;
         }
         else{
 
-          
+          inx = -1;
+          ipx = 1;
+
+          cout << "qui2" << endl;
+
+          if(reale > 0)
+            x = (((QWidget::width()*5)/12 *reale)) + QWidget::width()/2;
+          else
+            x = ((QWidget::width()*5)/12 *(1+reale)) + QWidget::width()/12;
         }
 
 
-      p.end();
+        //indici e posizione parte immaginaria
+
+        int y = 0;
+        int ipy = -1;
+        int iny = 1;
+
+        double abs_immaginaria = abs(immaginaria);
+
+        if(abs_immaginaria >= 1){
+
+          int aux1 = static_cast<int>(abs_immaginaria);
+
+          ipy = aux1*2;
+          iny = aux1*(-2);
+
+          if(immaginaria > 0)
+            y = ((QWidget::height()*5)/24) + QWidget::height()/12;
+          else
+            y = ((QWidget::height()*5)/8) + QWidget::height()/12;
+
+        }
+        else if (abs_immaginaria == 0){
+
+          iny = -1;
+          ipy = 1;
+
+          cout << "sono qui" << endl;
+
+          y = QWidget::height()/2;
+        }
+        else{
+
+          iny = -1;
+          ipy = 1;
+
+          cout << "ciao" << endl;
+
+          if(immaginaria > 0){
+            y = (((QWidget::height()*5)/12) * (1 - immaginaria)) + QWidget::height()/12;
+            cout << "immaginaria positiva" << endl;
+          }
+          else
+            y = (((QWidget::height()*5)/12) *immaginaria) + QWidget::height()/2;
+        }
+
+        //disegno punto e indici
+
+        p.drawText((QWidget::width()*11)/12,
+                    QWidget::height()/2 + 15,
+                    QString::number(ipx));
+        p.drawText( QWidget::width()/12,
+                    QWidget::height()/2 + 15,
+                    QString::number(inx));
+
+        p.drawText(QWidget::width()/2 + 5,
+                    QWidget::height()/12,
+                    QString::number(ipy));
+        p.drawText( QWidget::width()/2 + 5,
+                    (QWidget::height()*11)/12,
+                    QString::number(iny));
+
+        p.setPen(QPen(Qt::green, 12, Qt::SolidLine, Qt::RoundCap));
+        p.drawPoint(x,y);
+
+        cout << QWidget::width() << endl;
+        cout << x << endl;
+
+        p.end();
     }
 };
 
